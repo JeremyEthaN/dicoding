51110e75833f716436d585bb6e9eef747af69034
JeremyEthaN <142328009+JeremyEthaN@users.noreply.github.com>
2024-06-15 00:53:38 +0700

    Update graduate_dropout_prediction_app.py

--- graduate_dropout_prediction_app.py
+++ graduate_dropout_prediction_app.py
@@ -24,7 +24,7 @@ from prediction import prediction
 
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("https://github.com/JeremyEthaN/dicoding/raw/main/Belajar%20Penerapan%20Data%20Science/itb.png", width=130)
+    st.image("https://raw.githubusercontent.com/JeremyEthaN/dicoding/main/Belajar%20Penerapan%20Data%20Science/itb%20logo.png", width=100)
 with col2:
     st.header('Graduate / Dropout Prediction App (Prototype)')
 
